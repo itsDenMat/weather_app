a29d336f23b234d8137cc8a54a2d4ae0bf9a03a6
itsDenMat <dennismateo93@gmail.com>
2022-07-26 22:48:23 -0700

    Add JavaScript file

---
+++ style.js
@@ -0,0 +1,15 @@
+	// JQuery element to link HTML
+	var $temp = $("#temperature");
+	var $humd = $("#humidity");
+	var $wind = $("#wind-speed");
+	var $feels = $("#feels-like");
+	var $CityName = $("#city-name");
+	var $forcastList = $("#forcast-list");
+	var $historyList = $("#history");
+	var $welcome = $(".welcomeContainer");
+	var $errorMessage = $("#errorMessage");
+	var $todayForecastContainer = $("#todayForecastContainer");
+	var $historybox = $("#historybox");
+	var $uvIndex = $("#index-uv");
+	var $digitalClock = $("#DigitalCLOCK");
+	var $greeting = $("#greeting");
